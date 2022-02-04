4f07ec22d9fbccad7bea51ef3b71c886bdffe5fb
oren12321 <oren12321@gmail.com>
2022-02-05 01:02:21 +0200

    Set and optimize default image env variables

--- image-utils/step0_env.sh
+++ image-utils/step0_env.sh
@@ -12,45 +12,45 @@ echo "Set target release version"
 
 ARCH=arm64
 RELEASE=bionic
-REPO=http://mirror.coganng.com/ubuntu-ports
+# REPO=http://mirror.coganng.com/ubuntu-ports
 # REPO=http://mirror.misakamikoto.network/ubuntu-ports
 
 # If REPO is empty, http://ports.ubuntu.com/ubuntu-ports will be used
 
 # Use below script to get the fastest repo
-# if [ -z "${REPO}" ]
-# then
-#     REPO=$(./find_mirrors.sh arm64 bionic main speed | sort -k 1 | head -n 1 | awk '{print $2}')
-# fi
+if [ -z "${REPO}" ]
+then
+    REPO=$(./find_mirrors.sh arm64 bionic main speed | sort -k 1 | head -n 1 | awk '{print $2}')
+fi
 
 echo "Set target platform"
 
-JETSON_BOARD=jetson-nano-devkit
+JETSON_BOARD=jetson-tx2-devkit
 JETSON_STORAGE=mmcblk0p1
-JETSON_BOARD_IMG=jetson-nano
+JETSON_BOARD_IMG=jetson-tx2
 JETSON_BOARD_REV=300
-JETSON_PLAT=t210
+JETSON_PLAT=t186
 JETSON_REL=r32.6
-JETSON_BSP=jetson-210_linux_r32.6.1_aarch64.tbz2
-JETSON_BSP_URL=https://developer.nvidia.com/embedded/l4t/r32_release_v6.1/t210/jetson-210_linux_r32.6.1_aarch64.tbz2
+JETSON_BSP=jetson_linux_r32.6.1_aarch64.tbz2
+JETSON_BSP_URL=https://developer.nvidia.com/embedded/l4t/r32_release_v6.1/t186/jetson_linux_r32.6.1_aarch64.tbz2
 
 echo "Set target directories"
 
-ROOT_DIR=/home/vqtrong/jetson-custom/rootfs
-WORK_DIR=/home/vqtrong/jetson-custom/build
+ROOT_DIR="${HOME}/jetson-custom/rootfs"
+WORK_DIR="${HOME}/jetson-custom/build"
 
 echo "Set system users"
 
 ROOT_PWD=root
 
-JETSON_NAME=rover
+JETSON_NAME=jetson
 JETSON_USR=jetson
-JETSON_PWD=cccc
+JETSON_PWD=jetson
 
 echo "Set desktop manager"
 
 # leave it empty to not install any DE
-# JETSON_DESKTOP=
+JETSON_DESKTOP=
 
 # just a minimal desktop
 # JETSON_DESKTOP=openbox
@@ -59,15 +59,15 @@ echo "Set desktop manager"
 # JETSON_DESKTOP=lxde
 
 # look better and lightweight
-JETSON_DESKTOP=xubuntu
+# JETSON_DESKTOP=xubuntu
 
 # more similar to ubuntu
 # JETSON_DESKTOP=ubuntu-mate
 
 echo "Set network settings"
 
-WIFI_SSID="Your WiFi Acess Point"
-WIFI_PASS="Your WiFi Password"
+WIFI_SSID=jetson
+WIFI_PASS=jetson
 
 ##########
 echo "Prepare environment"
