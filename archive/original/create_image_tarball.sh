a9f0b1cf08d99bc44a6861a6ae83e9a8f9ad92b0
oren12321 <oren12321@gmail.com>
2022-02-15 15:41:52 +0200

    Remove archived scripts

--- archive/original/create_image_tarball.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source step0_env.sh && \
-./step1_make_rootfs.sh && \
-./step2_customize_rootfs.sh && \
-./step3_apply_bsp.sh && \
-tar -jcvf ${ART_DIR}/${JETSON_BOARD}_${RELEASE}_${JETSON_PLAT}_${JETSON_REL}_${JETSON_DESKTOP}.tbz2 ${ICACHE_DIR}/Linux_for_Tegra
-
